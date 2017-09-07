ad3520652aea052f80588983261d9c2de5d7d525
Steven Hobson-Campbell <xapphire13@live.com>
2017-09-06 23:36:23 -0700

    Refactoring web call

--- src/api.ts
+++ src/api.ts
@@ -4,72 +4,63 @@ import * as xml2js from "xml2js";
 import {Key, KeyState} from "./types";
 import {Component, Device, DeviceInfo} from "./Device";
 
-export function getInfo(device: Device): Promise<DeviceInfo> {
+enum HttpMethod {
+  Get = "GET",
+  Post = "POST"
+}
+
+function makeRequest(method: HttpMethod, host: string, path: string, data?: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    const req = http.get({
+    const req = http.request({
+      method: method,
       port: "8090",
-      host: device.ipAddress,
-      path: "/info",
+      host: host,
+      path,
       headers: {
         "Content-Type": "application/xml"
       }
     }, res => {
       res.on("data", (result: string) => {
-        if (res.statusCode === 200) {
-          xml2js.parseString(result, (err, {info}) => {
-            err ? reject(err) : resolve(<DeviceInfo>{
-              components: info.components[0].component.map((component: any) => (<Component>{
-                category: component.componentCategory && component.componentCategory[0],
-                serialNumber: component.serialNumber && component.serialNumber[0],
-                softwareVersion: component.softwareVersion && component.softwareVersion[0]
-              })),
-              deviceType: info.type[0],
-              margeAccountUuid: info.margeAccountUUID[0],
-              margeUrl: info.margeURL[0],
-              name: info.name[0],
-              networkInfo: {
-                ipAddress: info.networkInfo[0].ipAddress[0],
-                macAddress: info.networkInfo[0].macAddress[0]
-              }
-            });
-          });
-        } else {
+        res.statusCode === 200 ?
+          resolve(result) :
           reject({
             statusCode: res.statusCode,
             message: res.statusMessage
           });
-        }
       });
     });
 
     req.on("error", err => reject(err));
+
+    if (method === HttpMethod.Post && data) {
+      req.write(data);
+    }
+    req.end();
   });
 }
 
-export function setKey(device: Device, key: Key, state: KeyState): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const req = http.request({
-      method: "POST",
-      port: "8090",
-      host: device.ipAddress,
-      path: "/key",
-      headers: {
-        "Content-Type": "application/xml"
+export async function getInfo(device: Device): Promise<DeviceInfo> {
+  const info = await makeRequest(HttpMethod.Get, device.ipAddress, "/info");
+
+  return new Promise<DeviceInfo>((res, rej) => xml2js.parseString(info, (err, {info}) => {
+    err ? rej(err) : res(<DeviceInfo>{
+      components: info.components[0].component.map((component: any) => (<Component>{
+        category: component.componentCategory && component.componentCategory[0],
+        serialNumber: component.serialNumber && component.serialNumber[0],
+        softwareVersion: component.softwareVersion && component.softwareVersion[0]
+      })),
+      deviceType: info.type[0],
+      margeAccountUuid: info.margeAccountUUID[0],
+      margeUrl: info.margeURL[0],
+      name: info.name[0],
+      networkInfo: {
+        ipAddress: info.networkInfo[0].ipAddress[0],
+        macAddress: info.networkInfo[0].macAddress[0]
       }
-    }, res => {
-      res.on("data", () => {
-        res.statusCode === 200 ?
-          resolve() :
-          reject({
-            statusCode: res.statusCode,
-            message: res.statusMessage
-          });
-      });
     });
+  }));
+}
 
-    req.on("error", err => reject(err));
-
-    req.write(`<key state="${state}" sender="Gabbo">${key}</key>`);
-    req.end();
-  });
+export async function setKey(device: Device, key: Key, state: KeyState): Promise<void> {
+  await makeRequest(HttpMethod.Post, device.ipAddress, "/key", `<key state="${state}" sender="Gabbo">${key}</key>`);
 }
